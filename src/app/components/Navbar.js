e5ca6b604e88b07a9bfe03bb90d327051ec50313
deucenn <panix.black@web.de>
2024-11-10 20:03:34 +0100

    Build navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,8 +1,10 @@
 export default function NavBar() {
-    return (
-      <div className="font-mono text-2xl text-center border-b border-slate-800 flex items-center justify-center h-16">
-        <h1>cvcraft</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="font-mono text-2xl text-center border-b border-slate-800 flex items-center justify-center h-20">
+      <h1>
+        <span>cv</span>
+        <span className="text-slate-400">craft</span>
+      </h1>
+    </div>
+  );
+}
