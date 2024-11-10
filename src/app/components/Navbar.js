f57233f9a0bf4ee782dc86955609a806906e74f5
deucenn <panix.black@web.de>
2024-11-10 15:21:52 +0100

    Build Navbar

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+      <div className="font-mono text-2xl text-center border-b border-slate-800 flex items-center justify-center h-16">
+        <h1>cvcraft</h1>
+      </div>
+    );
+  }
+  
